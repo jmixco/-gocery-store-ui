66ea6bb70d13b8d29609c397b63d75c7daf68b1a
Julio Mixco <julio.mixg@gmail.com>
2020-02-12 18:37:25 -0600

    skip auth in login request

--- src/app/modules/core/services/auth.service.ts
+++ src/app/modules/core/services/auth.service.ts
@@ -1,17 +1,43 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { tap } from 'rxjs/operators';
+import { LoginResponse } from '../models/auth.model';
+import { Observable } from 'rxjs';
+import { SKIP_AUTH } from '../constants/auth.constant';
 
 @Injectable()
 export class AuthService {
+  isAuthenticated = false;
   constructor(private http: HttpClient) {}
-  login(userName: string, password: string, remember: boolean) {
+  login(
+    userName: string,
+    password: string,
+    remember: boolean
+  ): Observable<LoginResponse> {
+    const headers = new HttpHeaders({
+      [SKIP_AUTH]: JSON.stringify(true),
+    });
     return this.http
-      .post(`${environment.apiUrl}/identity/login`, {
-        email: userName,
-        password,
-      })
-      .pipe(tap((response) => console.log(response)));
+      .post<LoginResponse>(
+        `${environment.apiUrl}/identity/login`,
+        {
+          email: userName,
+          password,
+        },
+        {
+          headers,
+        }
+      )
+      .pipe(
+        tap((response) => {
+          localStorage.setItem(environment.tokenKey, response.token);
+          this.isAuthenticated = true;
+        })
+      );
+  }
+
+  retrieveToken() {
+    return localStorage.getItem(environment.tokenKey);
   }
 }
