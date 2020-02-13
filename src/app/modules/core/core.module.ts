808935a72e60234bde325ec7e5a1db35b24e28b6
Julio Mixco <jmixco@applaudostudios.com>
2020-02-12 18:36:02 -0600

    create authentication interceptor

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -3,11 +3,16 @@ import { AuthService } from './services/auth.service';
 import { MainLayoutComponent } from './components/main-layout/main-layout.component';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { RouterModule } from '@angular/router';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
 const components = [MainLayoutComponent];
 const services = [AuthService];
 @NgModule({
   declarations: [components],
-  providers: [services],
+  providers: [
+    services,
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+  ],
   imports: [RouterModule, NgZorroAntdModule],
 })
 export class CoreModule {}
