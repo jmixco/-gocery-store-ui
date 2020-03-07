a8198b52531ab74d7ff3e05ddc264e407c7e8b5e
Julio Mixco <jmixco@applaudostudios.com>
2020-03-07 11:58:32 -0600

    improve tests for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -34,4 +34,31 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
+
+  it('should navigate to login if sesion was not restored', () => {
+    authServiceSpy.tryToRestoreLogin.and.returnValue(false);
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+    expect(authServiceSpy.tryToRestoreLogin.calls.count()).toBe(
+      1,
+      'tryToRestoreLogin  called'
+    );
+    expect(routerSpy.navigate.calls.count()).toBe(1, 'navigate  called');
+    expect(routerSpy.navigate.calls.first().args[0]).toEqual(
+      ['/auth/login'],
+      'should nave to login'
+    );
+  });
+  it('should not navigate if sesion was restored', () => {
+    authServiceSpy.tryToRestoreLogin.and.returnValue(true);
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+    expect(authServiceSpy.tryToRestoreLogin.calls.count()).toBe(
+      1,
+      'tryToRestoreLogin called'
+    );
+    expect(routerSpy.navigate.calls.count()).toBe(0, 'navigate not called');
+  });
 });
